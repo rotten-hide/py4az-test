1acc1c01072fd66529dd8824f78c85ed7e39a7ca
rotten-hide <hidetake6991@gmail.com>
2018-02-10 01:41:11 +0900

    add: helloworldページ作成

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def template(request):
+    return render(request, 'app/template.html')
+
+def index(request):
+    context = {'test': 'test'}
+    return render(request, 'app/index.html')
